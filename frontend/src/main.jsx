d0790f1975833990c61f79ec51962df439d1b23d
Tobias Ocula <tobias.ocula@outlook.com>
2025-07-05 22:04:32 +0200

    fixed more stuffé

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
 import { ContextProvider } from './context.jsx';
 
 createRoot(document.getElementById('root')).render(
@@ -10,5 +9,5 @@ createRoot(document.getElementById('root')).render(
     <ContextProvider>
     <App />
     </ContextProvider>
-  </StrictMode>,
+  </StrictMode>
 )
